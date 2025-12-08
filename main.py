32a67d2c799cf13ebd73919f31877f652118c0a2
Sergey Efimov <sy365@yandex.ru>
2025-12-08 06:07:57 +0300

    Fix api

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ PhoneNumberType = typing.Annotated[
     typing.Union[str, phonenumbers.PhoneNumber], PhoneNumberValidator(number_format="E164")
 ]
 
-app = fastapi.FastAPI()
+app = fastapi.FastAPI(title="Phone Cache", version="1.0.0")
 config = config_lib.Config(".env")
 
 redis_client = redis.Redis(
@@ -19,43 +19,44 @@ redis_client = redis.Redis(
     db=config("REDIS_DB", cast=int, default=0))
 
 
-class Details(BaseModel):
+class Customer(BaseModel):
     address: str
+    phone: PhoneNumberType = None
 
 
-async def get_details(phone: PhoneNumberType) -> Details:
+async def get_details(phone: PhoneNumberType) -> Customer:
     address = await redis_client.get(phone)
     if not address:
         raise fastapi.HTTPException(status_code=404, detail="Phone not found")
 
-    return Details(address=address)
+    return Customer(address=address, phone=phone)
 
 
-@app.get("/", response_model=Details, responses={404: {}})
+@app.get("/{phone}", response_model=Customer, responses={404: {}})
 async def get(phone: PhoneNumberType):
     return await get_details(phone)
 
 
 @app.post("/", status_code=201)
-async def post(phone: PhoneNumberType, address) -> Details:
-    response = await redis_client.setnx(phone, address)
-    if not response:
+async def post(customer: Customer) -> Customer:
+    stored = await redis_client.setnx(customer.phone, customer.address)
+    if not stored:
         raise fastapi.HTTPException(status_code=409, detail="Duplicate entry")
 
-    return Details(address=address)
+    return customer
 
 
-@app.patch("/", responses={404: {}})
-async def post(phone: PhoneNumberType, address) -> Details:
-    updated = await redis_client.set(phone, address, xx=True)
+@app.put("/{phone}", responses={404: {}})
+async def post(phone: PhoneNumberType, customer: Customer) -> Customer:
+    updated = await redis_client.set(phone, customer.address, xx=True)
     if not updated:
         raise fastapi.HTTPException(status_code=404, detail="Phone not found")
 
-    return Details(address=address)
+    return Customer(phone=phone, address=customer.address)
 
 
-@app.delete("/")
-async def post(phone: PhoneNumberType) -> Details:
+@app.delete("/{phone}")
+async def post(phone: PhoneNumberType) -> Customer:
     details = await get_details(phone)
     await redis_client.delete(phone)
 
